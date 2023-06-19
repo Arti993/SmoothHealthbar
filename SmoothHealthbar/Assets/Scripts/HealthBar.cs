82fb893b24884178bdf5398b69060759a9d5eb39
Artem <artem.ulm@gmail.com>
2023-06-19 17:43:00 +0300

    Code fixing

--- SmoothHealthbar/Assets/Scripts/HealthBar.cs
+++ SmoothHealthbar/Assets/Scripts/HealthBar.cs
@@ -7,7 +7,9 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
     [SerializeField] private Player _player;
-    [SerializeField] float _healthChangeSpeed;
+    [SerializeField] private float _healthChangeSpeed;
+
+    private Coroutine _currentCoroutine;
 
     private float _nextSliderValue;
 
@@ -16,6 +18,7 @@ public class HealthBar : MonoBehaviour
         _player.HealthChanged += OnValueChanged;
         _slider.value = 1;
         _nextSliderValue = _slider.value;
+        _currentCoroutine = null;
     }
 
     private void OnDisable()
@@ -23,13 +26,26 @@ public class HealthBar : MonoBehaviour
         _player.HealthChanged -= OnValueChanged;
     }
 
-    private void Update()
-    {
-        _slider.value = Mathf.MoveTowards(_slider.value, _nextSliderValue, _healthChangeSpeed * Time.deltaTime);
-    }
-
     private void OnValueChanged(int value, int maxValue)
     {
-        _nextSliderValue = (float)value / maxValue;
+        _nextSliderValue = Mathf.Clamp((float)value / maxValue, 0f, 1f);
+
+        if(_currentCoroutine != null)
+        {
+            StopCoroutine(_currentCoroutine);
+        }
+
+        _currentCoroutine = StartCoroutine(ChangeSliderValue(_nextSliderValue));
+    }
+
+    private IEnumerator ChangeSliderValue(float nextSliderValue)
+    {
+        while(_slider.value != nextSliderValue)
+        {
+            _slider.value = Mathf.Clamp(Mathf.MoveTowards(_slider.value, _nextSliderValue, _healthChangeSpeed * Time.deltaTime), 0f, 1f);
+            yield return null;
+        }
+
+        _currentCoroutine = null;
     }
 }
